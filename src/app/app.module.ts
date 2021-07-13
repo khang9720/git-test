1f567dae07d41181c963ad281f4dfa4a7ca105cc
khang9720 <68835942+khang9720@users.noreply.github.com>
2021-07-13 15:43:16 +0700

    updat : conpomemt chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component'
 import { IconComponent } from './conponents/icon/icon.component'
 import { FormComponent } from './conponents/form/form.component'
 import { FooterComponent } from './conponents/footer/footer.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { ChartComponent } from './conponents/chart/chart.component'
 
 @NgModule({
-  declarations: [AppComponent, IconComponent, FormComponent, FooterComponent],
+  declarations: [AppComponent, IconComponent, FormComponent, FooterComponent, ChartComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

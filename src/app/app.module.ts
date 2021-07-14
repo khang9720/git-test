9469917eb5bcfc78b7492d12cf38b891a223d794
khang9720 <68835942+khang9720@users.noreply.github.com>
2021-07-14 13:50:11 +0700

    update charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core'
+
+import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
@@ -6,12 +8,18 @@ import { AppComponent } from './app.component'
 import { IconComponent } from './conponents/icon/icon.component'
 import { FormComponent } from './conponents/form/form.component'
 import { FooterComponent } from './conponents/footer/footer.component'
-import { FormsModule } from '@angular/forms';
 import { ChartComponent } from './conponents/chart/chart.component'
+import { ChartsModule } from 'ng2-charts'
 
 @NgModule({
-  declarations: [AppComponent, IconComponent, FormComponent, FooterComponent, ChartComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    IconComponent,
+    FormComponent,
+    FooterComponent,
+    ChartComponent,
+  ],
+  imports: [AppRoutingModule, ChartsModule, FormsModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/conponents/chart/chart.component.ts
+++ src/app/conponents/chart/chart.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import { ChartDataSets, ChartType, ChartOptions } from 'chart.js'
+import { Label } from 'ng2-charts'
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  styleUrls: ['./chart.component.css'],
 })
 export class ChartComponent implements OnInit {
+  // scatter
+  public scatterChartOptions: ChartOptions = {
+    responsive: true,
+  }
+  public scatterChartLabels: Label[] = [
+    'Eating',
+    'Drinking',
+    'Sleeping',
+    'Designing',
+    'Coding',
+    'Cycling',
+    'Running',
+  ]
 
-  constructor() { }
+  public scatterChartData: ChartDataSets[] = [
+    {
+      data: [
+        { x: 1, y: 1 },
+        { x: 2, y: 3 },
+        { x: 3, y: -2 },
+        { x: 4, y: 4 },
+        { x: 5, y: -3 },
+      ],
+      label: 'Series A',
+      pointRadius: 10,
+    },
+  ]
+  public scatterChartType: ChartType = 'scatter'
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event: MouseEvent
+    active: {}[]
+  }): void {
+    console.log(event, active)
   }
 
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event: MouseEvent
+    active: {}[]
+  }): void {
+    console.log(event, active)
+  }
 }
